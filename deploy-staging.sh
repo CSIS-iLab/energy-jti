c8e90252869750d99b452a8adde65584e4f8ff24
Jacque Schrag <jnschrag@gmail.com>
2020-10-05 21:53:57 -0400

    Update branch name in staging deploy script

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -4,8 +4,8 @@ ssh-add /tmp/csisjti_rsa
 git remote add csisjti-staging git@git.wpengine.com:production/csisjtistaging.git # add remote for staging site
 git fetch --unshallow # fetch all repo history or wpengine complain
 git status # check git status
-git checkout master # switch to master branch
+git checkout main # switch to main branch
 git add wp-content/themes/csisjti/*.css -f # force all compiled CSS files to be added
 git add wp-content/themes/csisjti/assets -f # force all compiled JS & optimized images
 git commit -m "Compiled & bundled all assets" # commit the compiled CSS files
-git push -f csisjti-staging master #deploy to staging site from master
+git push -f csisjti-staging main #deploy to staging site from main
