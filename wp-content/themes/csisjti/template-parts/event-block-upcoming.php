ed35d52e20e36b2a82144409c820b15682f3130f
Serven Maraghi <maraghiserven@gmail.com>
2020-10-31 17:10:57 -0400

    Complete upcoming event block

--- wp-content/themes/csisjti/template-parts/event-block-upcoming.php
+++ wp-content/themes/csisjti/template-parts/event-block-upcoming.php
@@ -18,18 +18,18 @@
 		<div class="event-block__date">
 			<?php $date = split_date(get_field('date_of_event')); ?>
 				 <p class="event-block__month"><?php echo $date[1] ?></p>
-				 <p class="event-block__day"><?php echo $date[0] ?></p>
-				 <p class="event-block__year"><?php echo $date[2] ?></p>
+				 <p class="event-block__day"><?php echo $date[2] ?></p>
+				 <p class="event-block__year"><?php echo $date[0] ?></p>
 			<?php endif; ?>
 		</div>
 
 		<?php if( get_field('time') ): ?>
-			<div class="event-block__time"><?php the_field('time'); ?></p>
+			<p class="event-block__time"><?php the_field('time'); ?></p>
 		<?php endif; ?>
 
 		<?php if( get_field('location') ): ?>
 			<?php $location = get_field('location', false, false); ?>
-				<p class="event-block__date__location"><?php echo $location; ?></p>
+				<p class="event-block__location"><?php echo $location; ?></p>
 		<?php endif; ?>
     
   </div>
