cc12b58ee151379348b4ba56fcc6ec37c230307c
Serven Maraghi <maraghiserven@gmail.com>
2020-11-03 13:21:30 -0500

    Remove repeated code in entry header

--- wp-content/themes/csisjti/template-parts/entry-header.php
+++ wp-content/themes/csisjti/template-parts/entry-header.php
@@ -15,31 +15,31 @@ $entry_header_classes = '';
     
 	<?php
 	csisjti_share();
+
+	the_title('<h1 class="entry-header__title">', '</h1>');
+
+	csisjti_header_description();
+
+	csisjti_display_categories();
+
+	csisjti_header_subtitle();
   
 	if (get_post_type() == 'event') {
-
-		csisjti_display_categories();
-
-		the_title('<h1 class="entry-header__title">', '</h1>');
-			
-		csisjti_header_subtitle();
-
-		csisjti_header_description();
 
 		csisjti_last_updated();
 
 		get_template_part( 'template-parts/event-block-upcoming' );
 	?>
-
 		<!-- past event block -->
 		 
-		<?php $related_analysis = get_field( 'related_analysis' ); ?>
-		<?php if ( $related_analysis ) : ?>
-			<?php $post = $related_analysis; ?>
-			<?php setup_postdata( $post ); ?> 
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
+		<?php $related_analysis = get_field( 'related_analysis' ); 
+		if ( $related_analysis ) : 
+			$post = $related_analysis;  
+		 	setup_postdata( $post ); ?>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			<?php wp_reset_postdata(); 
+		endif; 
+		?>
 
 	<?php } else if ( has_post_thumbnail() ) { ?>
 		<div class="post-block__img"><?php the_post_thumbnail( 'large' ); ?></div>
@@ -47,37 +47,17 @@ $entry_header_classes = '';
 		<?php
 		get_template_part( 'template-parts/featured-image-caption' );
 
-		csisjti_display_categories();
-
-		the_title( '<h1 class="entry-header__title">', '</h1>' );
-
-		csisjti_header_subtitle();
-
-		csisjti_header_description();
-
 		if (get_post_type() == 'post') { csisjti_posted_on(); }
 
 	} else if ((get_post_type() == 'resource-library')) {
 		
-		csisjti_display_categories();
-
-		the_title( '<h1 class="entry-header__title">', '</h1>' );
-
-		csisjti_header_description();
 	?>
-
 		<p class="entry-header__icon"><span><?php echo csisjti_get_svg('info'); ?></span>Classifications</p>
 
 		<a href="" class="cta cta--white">What is "Just Transition"? 
 			<?php echo csisjti_get_svg( 'arrow-right' ); ?>
 		</a>
 		
-	<?php } else {
-
-		the_title( '<h1 class="entry-header__title">', '</h1>' );
-
-		csisjti_header_description();
-
-	}?>
+	<?php } ?>
 
 </header><!-- .entry-header -->
