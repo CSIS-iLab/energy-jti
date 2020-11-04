6fb176e3bb1b875554e55a802deabb89bd0fde93
Serven Maraghi <maraghiserven@gmail.com>
2020-11-04 11:08:27 -0500

    Add all changes for testing purposes

---
+++ wp-content/themes/csisjti/assets/_js/custom/modal.js
@@ -0,0 +1,17 @@
+/**
+ * JS file for instantiating a11y dialogue instance
+ *
+ */
+// import A11yDialog from 'a11y-dialog';
+import tippy from 'tippy.js' 
+
+// document.getElementById('site-content').addEventListener('click', function () { 
+// // Get the dialog element (with the accessor method you want)
+// const el = document.getElementById('my-accessible-dialog');
+
+// // Instantiate a new A11yDialog module
+// const dialog = new A11yDialog(el);
+
+// })
+
+console.log('helloworld')
