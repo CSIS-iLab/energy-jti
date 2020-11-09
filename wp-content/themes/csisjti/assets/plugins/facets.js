1a47d8fcffd802c327fd1597a85e85a2aad18928
Serven Maraghi <maraghiserven@gmail.com>
2020-11-08 23:01:12 -0500

    Add styling for checkboxes

--- wp-content/themes/csisjti/assets/plugins/facets.js
+++ wp-content/themes/csisjti/assets/plugins/facets.js
@@ -1,3 +1,26 @@
+// ;(function ($) {
+//   $(document).on('facetwp-loaded', function () {
+//     modifyFSelectFacet()
+//   })
+
+//   function modifyFSelectFacet() {
+//     console.log(FWP)
+
+//     $('.facetwp-checkbox').each(function () {
+//       const $facet = $(this)
+//       const facet_name = $facet.attr('data-value')
+//       const facet_label = facet_name
+//       console.log(this)
+//       // Add Facet Label
+//       const label = document.createElement('div')
+//       label.classList.add('fs-label-field')
+//       label.innerHTML = facet_label
+//       console.log(this)
+//       this.prepend(label)
+//     })
+//   }
+// })(jQuery)
+
 ;(function ($) {
   $(document).on('facetwp-loaded', function () {
     modifyFSelectFacet()
@@ -6,21 +29,12 @@
   function modifyFSelectFacet() {
     console.log(FWP)
 
-    $('.facetwp-type-checkboxes').each(function () {
-      const $facet = $(this)
-      const facet_name = $facet.attr('data-name')
-      const facet_label = FWP.settings.labels[facet_name]
-      console.log(this)
-      // Add Facet Label
-      const label = document.createElement('div')
-      label.classList.add('fs-label-field')
-      label.innerHTML = facet_label
-      console.log(this)
-      this.querySelector('.fs-label-wrap').prepend(label)
+    $('.facetwp-checkbox').each(function () {
+      const span = document.createElement('span')
+      span.classList.add('fs-checkbox')
+      span.innerHTML = '<i></i>'
 
-      // Add Number of Selected Options to Wrapper
-      const numSelected = FWP.facets[facet_name].length
-      this.querySelector('.fs-label-wrap').setAttribute('data-num', numSelected)
+      this.prepend(span)
     })
   }
 })(jQuery)
