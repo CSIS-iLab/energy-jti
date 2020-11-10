072455ad958766a089024ea0e9565fb3ad0b1a71
Jacque Schrag <jnschrag@gmail.com>
2020-11-10 15:57:39 -0500

    Add top bar to resource library

--- wp-content/themes/csisjti/archive-resource-library.php
+++ wp-content/themes/csisjti/archive-resource-library.php
@@ -16,38 +16,49 @@ get_header();
 
 <main id="site-content" role="main">
 	<?php get_template_part( 'template-parts/entry-header' ); ?>
+	<div class="resource-library__top-bar"></div>
+	<div class="resource-library__content">
+		<div class="resource-library__secondary">
+			<div class="resource-library__sticky">
 
-	Tabs go in header component.
+				<div class="resource-library__pagination">
+				<?php echo facetwp_display( 'facet', 'pagination' ); ?>
+
+					<button id="mobile-filters-btn" class="btn btn--outline btn--round btn--filters" data-a11y-dialog-show="accessible-dialog">
+						<?php
+							echo csisjti_get_svg( 'filter' );
+						?>
+						Filters
+					</button>
+				</div>
+
+				<div class="resource-library__inline-filters">
+					<h2 class="resource-library__subheading">Search &amp; Filter</h2>
+					<?php
+						echo facetwp_display( 'facet', 'search_input' );
+						echo facetwp_display( 'facet', 'analysis_type' );
+						echo facetwp_display( 'facet', 'geographic_focus' );
+						echo facetwp_display( 'facet', 'sectors' );
+						echo facetwp_display( 'facet', 'focus_areas' );
+						echo facetwp_display( 'facet', 'format' );
+					?>
+					<button id="filters-btn" class="btn btn--outline btn--round btn--filters" data-a11y-dialog-show="accessible-dialog">
+						<?php
+							echo csisjti_get_svg( 'filter' );
+						?>
+						More Filters
+					</button>
+					<button id="filters-reset-btn" class="btn--reset" onclick="FWP.reset()">Clear All</button>
+				</div>
+			</div>
+		</div>
 
 		<div class="filters-overview">
-			<?php echo facetwp_display( 'facet', 'pagination' ); ?>
-			Filters Button (Mobile)<br />
 			<div class="filters-overview__totals">
 				<strong class="is-highlighted" id="num_filters_applied">0</strong>&nbsp;filters applied,&nbsp;
 				<?php echo facetwp_display( 'facet', 'total_entries' ); ?>
 			</div>
 			<?php echo facetwp_display( 'sort' ); ?>
-		</div>
-
-	<div class="resource-library__content">
-
-		<div class="resource-library__secondary">
-			<h2 class="resource-library__subheading">Search &amp; Filter</h2>
-			<?php
-				echo facetwp_display( 'facet', 'search_input' );
-				echo facetwp_display( 'facet', 'analysis_type' );
-				echo facetwp_display( 'facet', 'geographic_focus' );
-				echo facetwp_display( 'facet', 'sectors' );
-				echo facetwp_display( 'facet', 'focus_areas' );
-				echo facetwp_display( 'facet', 'format' );
-			?>
-			<button id="filters-btn" class="btn btn--outline btn--round btn--filters" data-a11y-dialog-show="accessible-dialog">
-				<?php
-					echo csisjti_get_svg( 'filter' );
-				?>
-				More Filters
-			</button>
-			<button id="filters-reset-btn" class="btn--reset" onclick="FWP.reset()">Clear All</button>
 		</div>
 
 		<div class="resource-library__results">
--- wp-content/themes/csisjti/inc/template-functions.php
+++ wp-content/themes/csisjti/inc/template-functions.php
@@ -505,7 +505,7 @@ function set_posts_per_page( $query ) {
   global $wp_the_query;
 
   if ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_archive() ) ) {
-    $query->set( 'posts_per_page', 1 );
+    $query->set( 'posts_per_page', 2 );
   }
 
   return $query;
