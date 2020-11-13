7b287cceb0914e9a15f79a05aa8ad1815ea4677b
Jacque Schrag <jnschrag@gmail.com>
2020-11-13 17:41:16 -0500

    Organize & tweak typography settings

--- wp-content/themes/csisjti/inc/custom-blocks.php
+++ wp-content/themes/csisjti/inc/custom-blocks.php
@@ -7,6 +7,13 @@
  * @since 1.0.0
  */
 
+add_action('init', function() {
+	register_block_style('core/media-text', [
+		'name' => 'csisjti-participants',
+		'label' => 'Participants'
+	]);
+});
+
 // Set Download Button to have alignsmall class always.
 function csisjti_lzb_block_download_button_render_attributes( $attributes, $content, $block, $context ) {
     $attributes['className'] .= ' alignsmall';
