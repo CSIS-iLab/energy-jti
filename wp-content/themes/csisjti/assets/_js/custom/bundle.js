1dcd04eb8f650c92bcd2f489a20fc664132bba48
Serven Maraghi <maraghiserven@gmail.com>
2020-11-16 09:30:37 -0500

    Set up tippy tooltip

--- wp-content/themes/csisjti/assets/_js/custom/bundle.js
+++ wp-content/themes/csisjti/assets/_js/custom/bundle.js
@@ -1,7 +1,9 @@
 import { Navigation } from './navbar'
 import { setScrollbarSize } from './scrollbar'
+import { Tooltip } from './tooltip'
 
 document.addEventListener('DOMContentLoaded', function () {
   setScrollbarSize()
   Navigation()
+  Tooltip()
 })
---
+++ wp-content/themes/csisjti/assets/_js/custom/tooltip.js
@@ -0,0 +1,12 @@
+/**
+ * JS file for instantiating a11y dialogue instance
+ *
+ */
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css' // optional for styling
+
+const Tooltip = () => {
+  tippy('.filters-modal__subheading')
+}
+
+export { Tooltip }
