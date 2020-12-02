3c4abacc57b878f6662a1d50cb3dad89a334528b
Serven Maraghi <maraghiserven@gmail.com>
2020-12-02 15:37:49 -0500

    Remove position sticky from side panel and pagination

--- wp-content/themes/csisjti/archive-resource-library.php
+++ wp-content/themes/csisjti/archive-resource-library.php
@@ -19,7 +19,7 @@ get_header();
 	<div class="resource-library__top-bar"></div>
 	<div class="resource-library__content">
 		<div class="resource-library__secondary">
-			<div class="resource-library__sticky">
+			<div class="resource-library__side-panel">
 
 				<div class="resource-library__pagination">
 				<?php echo facetwp_display( 'facet', 'pagination' ); ?>
